0badfd57293ddab23e2d7e6aeba7c788ad498d99
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2023-11-07 18:37:22 +0530

    Initialised a default app using material ui templates
    created base app from mui templates
    https://github.com/mui/material-ui/tree/master/examples
    
    templates
    https://mui.com/material-ui/getting-started/templates/

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -5,7 +5,7 @@ import Link from '@mui/material/Link';
 import Slider from '@mui/material/Slider';
 import PopoverMenu from './PopoverMenu';
 import ProTip from './ProTip';
-
+import Router from "./pages";
 function Copyright() {
   return (
     <Typography variant="body2" color="text.secondary" align="center">
@@ -21,21 +21,9 @@ function Copyright() {
 
 export default function App() {
   return (
-    <Container maxWidth="sm">
-      <div className="my-4">
-        <Typography variant="h4" component="h1" gutterBottom>
-          Material UI Create React App example with Tailwind CSS in TypeScript
-        </Typography>
-        <Slider
-          className="my-4"
-          defaultValue={30}
-          classes={{ active: 'shadow-none' }}
-          slotProps={{ thumb: { className: 'hover:shadow-none' } }}
-        />
-        <PopoverMenu />
-        <ProTip />
-        <Copyright />
-      </div>
+    <Container maxWidth="lg">
+      <Router />
+
     </Container>
   );
 }
---
+++ web-app/src/pages/index.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import SignIn from "./SignIn";
+import Blog from "./Blog/Blog";
+import Dashboard from "./Dashboard";
+import Pricing from "./Pricing";
+import Products from "./Products";
+import SignUp from "./SignUp";
+
+// Sample components for different routes
+const Home = () => <div>
+  <nav>
+    <ul>
+      <li>
+        <Link to="/sign-in">Signin</Link>
+      </li>
+      <li>
+        <Link to="/home">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+      <li>
+        <Link to="/contact">Contact</Link>
+      </li>
+      <li>
+        <Link to="/blog">blog</Link>
+      </li> <li>
+        <Link to="/dashboard">dashboard</Link>
+      </li> <li>
+        <Link to="/pricing">pricing</Link>
+      </li> <li>
+        <Link to="/products">products</Link>
+      </li>
+      <li>
+        <Link to="/sign-up">sign-up</Link>
+      </li>
+
+    </ul>
+  </nav>
+</div>;
+const About = () => <div>About</div>;
+const Contact = () => <div>Contact</div>;
+
+function App() {
+  return (
+    <Router>
+      <div>
+
+        <li>
+          <Link to="/home">Home</Link>
+        </li>
+        <Routes>
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/" element={<Home />} />
+
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/sign-up" element={<SignUp />} />
+
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
